7bc7b5186c073b14519c37d77ed4243d9bf54538
Lev <levkorotich@gmail.com>
2025-06-01 22:50:17 +1000

    Yandex Metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -160,6 +161,36 @@ export default function RootLayout({
         <meta name="yandex-verification" content="dce0f9d81c096939" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
+        {/* Yandex.Metrika counter - Main script */}
+        <Script
+          id="yandex-metrika-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}n
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(102318371, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });
+            `,
+          }}
+        />
+        {/* Yandex.Metrika counter - noscript tag */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/102318371" alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
